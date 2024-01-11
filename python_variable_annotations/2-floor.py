ecca9b27eb815f34f68608d8b1fc435e57763ccb
HaroldS10 <haroldsanty13@gmail.com>
2024-01-11 11:20:47 -0500

    upload 1 progress of the FLOOR function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+function takes a float and returns n as integer
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    function takes a float and returns n as integer
+    '''
+    return int(n)
