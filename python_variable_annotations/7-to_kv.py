298b5f392ec98ef0ad3dd779024252a9570f1e52
HaroldS10 <haroldsanty13@gmail.com>
2024-01-11 12:15:35 -0500

    upload 1 progress of the to_kv function

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+program has a single function
+'''
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    function returns Tuple, first str, second float
+    of square value of v
+    '''
+    return (k, v*v)
