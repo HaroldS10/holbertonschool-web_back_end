de7e2378bcbd698f8b233d2bc2a9fdf502139843
HaroldS10 <haroldsanty13@gmail.com>
2024-01-25 10:06:58 -0500

    upload first progress of file 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@ containing a start index and an end index corresponding,
 to the range of indexes to return in a list for those particular pagination parameters.
 """
 
-from typing import Tuple
+from typing import tuple
 
 def index_range (page: int, page_size: int) -> tuple [int, int]:
     
