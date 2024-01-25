753b7cb99b19c1bf8f867fa72450fa9050411111
HaroldS10 <haroldsanty13@gmail.com>
2024-01-25 10:14:23 -0500

    upload first progress of file 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ pagination index function
 from typing import Tuple
 
 
-def index_range (page: int, page_size: int) -> Tuple [int, int]:
+def index_range(page: int, page_size: int) -> Tuple [int, int]:
     
     ind_start = (page - 1) * page_size
     ind_end = page * page_size
