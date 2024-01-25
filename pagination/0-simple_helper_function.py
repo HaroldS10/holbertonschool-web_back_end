7fed2333ed77316e221ed09878464000734a7503
HaroldS10 <haroldsanty13@gmail.com>
2024-01-25 10:18:03 -0500

    upload first progress of file 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,6 +8,9 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ 
+    Retrieves the index range from a given page and page size.
+    """
 
     ind_start = (page - 1) * page_size
     ind_end = page * page_size
