ee3a5959c14b4c191ff63ed0b951ef409c4982c3
HaroldS10 <haroldsanty13@gmail.com>
2024-01-31 09:26:01 -0500

    upload file progress 0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,17 +1,13 @@
 export default class ClassRoom {
-    
+  constructor(maxStudentsSize) {
+    this.maxStudentsSize = maxStudentsSize;
+  }
 
-    constructor(maxStudentsSize) {
-        this.maxStudentsSize = maxStudentsSize;
-    }
+  set maxStudentsSize(value) {
+    this._maxStudentsSize = value;
+  }
 
-    set maxStudentsSize(value) {
-        this._maxStudentsSize = value;
-    }
-
-    get maxStudentsSize() {
-        return this._maxStudentsSize;
-    }
-
-
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
 }
