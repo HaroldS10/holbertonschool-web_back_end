ed97f6bbeebf6acd4fc41baf30a8de7969f262ea
HaroldS10 <haroldsanty13@gmail.com>
2024-02-08 15:26:56 -0500

    upload second  progress to file 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,9 @@
-export default function updateStudentGradeByCity(students, city, newGrades){ 
-    return students 
-    .filter(students => students.location === city)
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((students) => students.location === city)
     .map((students) => {
-        const getID = newGrades.find(grade => grade.studentId === students.id)
-        const addGrade = getID ? getID.grade : 'N/A';
-        return { ...students, grade: addGrade}
-    })
+      const getID = newGrades.find((grade) => grade.studentId === students.id);
+      const addGrade = getID ? getID.grade : 'N/A';
+      return { ...students, grade: addGrade };
+    });
 }
