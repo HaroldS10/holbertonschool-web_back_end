a118ed4519e458e7a0d3e57e5b4d6d17e70d293c
HaroldS10 <haroldsanty13@gmail.com>
2024-02-09 08:56:16 -0500

    upload syntax fixes for file 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
 export default function cleanSet(set, startString) {
   if (set instanceof Set && typeof startString === 'string') {
     return Array.from(set)
-      .filter(value => typeof value === 'string' && value.startsWith(startString))
-      .map(value => value.slice(startString.length))
+      .filter((value) => typeof value === 'string' && value.startsWith(startString))
+      .map((value) => value.slice(startString.length))
       .join('-');
-  } else {
-    return '';
   }
+  return '';
 }
